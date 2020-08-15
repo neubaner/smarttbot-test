f5f0ab1b36f7d65f19a1697f1c3de4e87f201ec3
Guilherme Neubaner <guilherme.neubaner@gmail.com>
2020-08-15 20:46:46 -0300

    Interface básica da API da Poloniex

---
+++ src/api/poloniex.test.ts
@@ -0,0 +1,29 @@
+import { fetchTicker, fetchCurrencyInfo, Ticker } from './poloniex'
+
+describe('fetchTicker', () => {
+  let ticker: Ticker[]
+
+  beforeAll(async () => {
+    ticker = await fetchTicker()
+  })
+
+  it('should return valid ticker data', async () => {
+    expect(Array.isArray(ticker)).toBe(true)
+    expect(ticker.length).toBeGreaterThan(0)
+  })
+
+  it('should have a valid currencyPair property', async () => {
+    const currencyPair = ticker[0].currencyPair
+
+    expect(Array.isArray(currencyPair)).toBe(true)
+    expect(currencyPair.length).toBe(2)
+  })
+})
+
+describe('fetchCurrencyInfo', () => {
+  it('should return valid currency info', async () => {
+    const result = await fetchCurrencyInfo()
+
+    expect(Object.keys(result).length).toBeGreaterThan(0)
+  })
+})
---
+++ src/api/poloniex.ts
@@ -0,0 +1,44 @@
+export interface Currency {
+  name: string
+  txFee: string
+  minConf: number
+  disabled: number
+  delisted: number
+  frozen: number
+}
+
+export interface CurrencyInfoResponse {
+  [key: string]: Currency
+}
+
+export interface Ticker {
+  currencyPair: [string, string]
+  last: string
+  lowestAsk: string
+  highestBid: string
+  percentChange: string
+  baseVolume: string
+  quoteVolume: string
+  isFrozen: number
+  high24hr: string
+  low24hr: string
+}
+
+const baseApiUrl = process.env.REACT_APP_POLONIEX_BASE_URL
+
+export async function fetchCurrencyInfo(): Promise<CurrencyInfoResponse> {
+  const response = await fetch(`${baseApiUrl}/public?command=returnCurrencies`)
+  const responseData = await response.json()
+
+  return responseData as CurrencyInfoResponse
+}
+
+export async function fetchTicker(): Promise<Ticker[]> {
+  const response = await fetch(`${baseApiUrl}/public?command=returnTicker`)
+  const responseData = await response.json()
+
+  return Object.keys(responseData).map((ticker) => ({
+    currencyPair: ticker.split('_'),
+    ...responseData[ticker],
+  }))
+}
